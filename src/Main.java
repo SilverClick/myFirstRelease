ec39bf2769e05f4788150c01f294147283522be5
Gonzalo Arca Iglesias <garcaiglesias@danielcastelao.org>
2022-10-21 09:44:17 +0200

    ahi va el main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+    Funciones calculadora = new Funciones();
+    System.out.println(calculadora.suma(2,3)););
     }
 }
